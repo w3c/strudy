a33e2f392f5d2a6dcc579656129a6b0cc5d64448
François Daoust <fd@w3.org>
2023-04-06 22:39:09 +0200

    Fix study-webidl: re-entrance and exposure. Add first tests (#317)
    The report object was global, so a second call to studyWebIdl would not forget
    about previous results.
    
    Also fixes #314.
    
    First few tests added, along with a job test.
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>

--- src/lib/study-webidl.js
+++ src/lib/study-webidl.js
@@ -2,125 +2,124 @@ const { expandCrawlResult } = require("reffy");
 const fetch = require("node-fetch");
 const WebIDL2 = require("webidl2");
 
-const report = {};
-
-// DRY with study-backref
-function recordAnomaly(spec, anomalyType, error) {
-  if (!report[spec.url]) {
-    report[spec.url] = {
-      title: spec.title,
-      crawled: spec.crawled,
-      shortname: spec.shortname,
-      repo: spec.nightly.repository,
-      // specific to this report
-      invalidWebIdl: [],
-      idlNameReused: [],
-      noMainIdlDefinition: [],
-      noIdlExposure: [],
-      incompatiblePartialIdlExposure: [],
-      duplicateIdlDefinition: [],
-      unexpectedEventHandler: [],
-      singleEnumValue: [],
-      wrongCaseEnumValue: []
-    };
-  }
-  report[spec.url][anomalyType].push(error);
-}
-
-const platformIdl = {};
-const globals = {};
-
-function inheritsFrom(iface, ancestor) {
-  if (!iface.inheritance) return false;
-  if (iface.inheritance === ancestor) return true;
-  const parentInterface = platformIdl[iface.inheritance].find(({idl}) => !idl.partial).idl;
-  return inheritsFrom(parentInterface, ancestor);
-}
-
-// TODO: a full check of event handlers would require:
-// - checking if the interface doesn't include a mixin with eventhandlers
-function checkEventHandlers(spec, memberHolder, iface = memberHolder) {
-  const eventHandler = memberHolder.members.find(m => m?.name?.startsWith("on") && m.type === "attribute" && m.idlType?.idlType === "EventHandler");
-  if (eventHandler && !inheritsFrom(iface, "EventTarget")) {
-    recordAnomaly(spec, "unexpectedEventHandler", `${iface.name} has an event handler ${eventHandler.name} but does not inherit from EventTarget`);
-  }
-}
-
-function getExposure(iface) {
-  let exposure = iface.extAttrs.find(ea => ea.name === "Exposed")?.rhs;
-  if (!exposure) return [];
-  if (exposure?.type === "*") {
-    exposure = Object.keys(globals);
-  } else {
-    if (exposure?.type === "identifier-list") {
-      exposure = exposure.value.map(({value}) => value);
-    } else if (exposure?.type === "identifier") {
-      exposure = [exposure.value];
-    }
-    // Expand encompassing globals to facilitate comparison
-    let additionalExposures = [];
-    for (let value of exposure) {
-      if (globals[value].subrealms.length) {
-	additionalExposures = additionalExposures.concat(globals[value].subrealms);
-      }
-    }
-    // remove duplicate values
-    exposure = [...new Set(exposure.concat(additionalExposures))];
-  }
-  return exposure;
-}
-
-function checkExposure(spec, iface, mainInterface) {
-  const mainExposure = getExposure(mainInterface);
-  if (iface.partial) {
-    // check exposure of partial is compatible with main
-    const partialExposure = getExposure(iface);
-    if (!mainExposure && partialExposure) {
-      recordAnomaly(spec, "incompatiblePartialIdlExposure", `partial interface ${iface.name} exposure (${partialExposure}) is incompatible with main exposure (${mainExposure})`);
-    } else if (partialExposure && mainExposure[0] !== "*" && partialExposure.some(x => !mainExposure.includes(x))) {
-      recordAnomaly(spec, "incompatiblePartialIdlExposure", `partial interface ${iface.name} exposure (${partialExposure}) is incompatible with main exposure (${mainExposure})`);
-    }
-  } else {
-    // check an exposure is defined and matches a well-known scope
-    if (!mainExposure) {
-      recordAnomaly(spec, "noIdlExposure", `Interface ${mainInterface.name} has no [Exposed] extended attribute`);
-    }
-  }
-}
-
-function checkEnumMultipleValues(spec, idl) {
-  if (idl.values.length <= 1) {
-    recordAnomaly(spec, "singleEnumValue", `The ${idl.name} enum has fewer than 2 possible values`);
-  }
-}
-
-function checkSyntaxOfEnumValue(spec, idl) {
-  idl.values.forEach(({value}) => {
-    if (value.match(/[A-Z]_/)) {
-      recordAnomaly(spec, "wrongCaseEnumValue", `The value ${value} of  ${idl.name} enum does not match the expected conventions (lower case, hyphen separated words)`);
-    }
-  });
-}
-
-
 const specName = ({spec}) => spec.shortname;
 
 async function studyWebIdl(edResults) {
+  const report = {};
+
+  // DRY with study-backref
+  function recordAnomaly(spec, anomalyType, error) {
+    if (!report[spec.url]) {
+      report[spec.url] = {
+        title: spec.title,
+        crawled: spec.crawled,
+        shortname: spec.shortname,
+        repo: spec.nightly.repository,
+        // specific to this report
+        invalidWebIdl: [],
+        idlNameReused: [],
+        noMainIdlDefinition: [],
+        noIdlExposure: [],
+        incompatiblePartialIdlExposure: [],
+        duplicateIdlDefinition: [],
+        unexpectedEventHandler: [],
+        singleEnumValue: [],
+        wrongCaseEnumValue: []
+      };
+    }
+    report[spec.url][anomalyType].push(error);
+  }
+
+  const platformIdl = {};
+  const globals = {};
+
+  function inheritsFrom(iface, ancestor) {
+    if (!iface.inheritance) return false;
+    if (iface.inheritance === ancestor) return true;
+    const parentInterface = platformIdl[iface.inheritance].find(({idl}) => !idl.partial).idl;
+    return inheritsFrom(parentInterface, ancestor);
+  }
+
+  // TODO: a full check of event handlers would require:
+  // - checking if the interface doesn't include a mixin with eventhandlers
+  function checkEventHandlers(spec, memberHolder, iface = memberHolder) {
+    const eventHandler = memberHolder.members.find(m => m?.name?.startsWith("on") && m.type === "attribute" && m.idlType?.idlType === "EventHandler");
+    if (eventHandler && !inheritsFrom(iface, "EventTarget")) {
+      recordAnomaly(spec, "unexpectedEventHandler", `${iface.name} has an event handler ${eventHandler.name} but does not inherit from EventTarget`);
+    }
+  }
+
+  function getExposure(iface) {
+    let exposure = iface.extAttrs.find(ea => ea.name === "Exposed")?.rhs;
+    if (!exposure) return [];
+    if (exposure?.type === "*") {
+      exposure = Object.keys(globals);
+    } else {
+      if (exposure?.type === "identifier-list") {
+        exposure = exposure.value.map(({value}) => value);
+      } else if (exposure?.type === "identifier") {
+        exposure = [exposure.value];
+      }
+      // Expand encompassing globals to facilitate comparison
+      let additionalExposures = [];
+      for (let value of exposure) {
+        if (globals[value].subrealms.length) {
+          additionalExposures = additionalExposures.concat(globals[value].subrealms);
+        }
+      }
+      // remove duplicate values
+      exposure = [...new Set(exposure.concat(additionalExposures))];
+    }
+    return exposure;
+  }
+
+  function checkExposure(spec, iface, mainInterface) {
+    const mainExposure = getExposure(mainInterface);
+    if (iface.partial) {
+      // check exposure of partial is compatible with main
+      const partialExposure = getExposure(iface);
+      if (mainExposure.length === 0 && partialExposure) {
+        recordAnomaly(spec, "incompatiblePartialIdlExposure", `partial interface ${iface.name} exposure (${partialExposure}) is incompatible with main exposure (${mainExposure})`);
+      } else if (partialExposure && mainExposure[0] !== "*" && partialExposure.some(x => !mainExposure.includes(x))) {
+        recordAnomaly(spec, "incompatiblePartialIdlExposure", `partial interface ${iface.name} exposure (${partialExposure}) is incompatible with main exposure (${mainExposure})`);
+      }
+    } else {
+      // check an exposure is defined and matches a well-known scope
+      if (mainExposure.length === 0) {
+        recordAnomaly(spec, "noIdlExposure", `Interface ${mainInterface.name} has no [Exposed] extended attribute`);
+      }
+    }
+  }
+
+  function checkEnumMultipleValues(spec, idl) {
+    if (idl.values.length <= 1) {
+      recordAnomaly(spec, "singleEnumValue", `The ${idl.name} enum has fewer than 2 possible values`);
+    }
+  }
+
+  function checkSyntaxOfEnumValue(spec, idl) {
+    idl.values.forEach(({value}) => {
+      if (value.match(/[A-Z]_/)) {
+        recordAnomaly(spec, "wrongCaseEnumValue", `The value ${value} of  ${idl.name} enum does not match the expected conventions (lower case, hyphen separated words)`);
+      }
+    });
+  }
+
   edResults.forEach(spec => {
     if (!spec.idl) return;
     let ast;
     try {
       ast = WebIDL2.parse(spec.idl);
       for (let idlItem of ast) {
-	if (!platformIdl[idlItem.name]) {
-	  platformIdl[idlItem.name] = [];
-	}
-	platformIdl[idlItem.name].push({spec, idl: idlItem});
-	let globalEA;
-	if (idlItem.type === "interface" && (globalEA = idlItem?.extAttrs?.find(ea => ea.name === "Global"))) {
-	  // mostly copied from webidlpedia - DRY?
-	  const globalValues = Array.isArray(globalEA.rhs.value) ? globalEA.rhs.value.map(({value}) => value) : [globalEA.rhs.value];
-	  for (const value of globalValues)  {
+        if (!platformIdl[idlItem.name]) {
+          platformIdl[idlItem.name] = [];
+        }
+        platformIdl[idlItem.name].push({spec, idl: idlItem});
+        let globalEA;
+        if (idlItem.type === "interface" && (globalEA = idlItem?.extAttrs?.find(ea => ea.name === "Global"))) {
+          // mostly copied from webidlpedia - DRY?
+          const globalValues = Array.isArray(globalEA.rhs.value) ? globalEA.rhs.value.map(({value}) => value) : [globalEA.rhs.value];
+          for (const value of globalValues)  {
             // '*' is not a name
             if (value === '*') break;
             if (!globals[value]) {
@@ -128,7 +127,7 @@ async function studyWebIdl(edResults) {
             }
             globals[value].components.push(idlItem.name);
           }
-	}
+        }
       }
     } catch (e) {
       // TODO: load from curated?
@@ -161,31 +160,31 @@ async function studyWebIdl(edResults) {
     const allowMultipleDefs = allowPartials.concat("includes");
     if (!allowMultipleDefs.includes(type)) {
       if (platformIdl[name].length > 1) {
-	recordAnomaly(platformIdl[name][0].spec, "duplicateIdlDefinition", `${name} is defined as ${type} in several specifications ${specNames.join(', ')}`);
+        recordAnomaly(platformIdl[name][0].spec, "duplicateIdlDefinition", `${name} is defined as ${type} in several specifications ${specNames.join(', ')}`);
       } else {
-	mainDef = platformIdl[name][0].idl;
+        mainDef = platformIdl[name][0].idl;
       }
     }
     if (allowPartials.includes(type)) {
       const mainDefs = platformIdl[name].filter(({idl}) => !idl.partial);
       if (mainDefs.length === 0) {
-	recordAnomaly(platformIdl[name][0].spec, "noMainIdlDefinition", `${name} is only defined as partial ${type} (in specifications ${platformIdl[name].map(specName).join(', ')})`);
-	continue;
+        recordAnomaly(platformIdl[name][0].spec, "noMainIdlDefinition", `${name} is only defined as partial ${type} (in specifications ${platformIdl[name].map(specName).join(', ')})`);
+        continue;
       } else if (mainDefs.length > 1) {
-	recordAnomaly(platformIdl[name][0].spec, "duplicateIdlDefinition", `${name} is defined as non-partial ${type} in several specifications ${mainDefs.map(specName).join(', ')}`);
+        recordAnomaly(platformIdl[name][0].spec, "duplicateIdlDefinition", `${name} is defined as non-partial ${type} in several specifications ${mainDefs.map(specName).join(', ')}`);
       }
       mainDef = mainDefs[0].idl;
     }
     for (let {spec, idl} of platformIdl[name]) {
       switch(idl.type) {
       case "interface":
-	checkEventHandlers(spec, idl, mainDef);
-	checkExposure(spec, idl, mainDef);
-	break;
+        checkEventHandlers(spec, idl, mainDef);
+        checkExposure(spec, idl, mainDef);
+        break;
       case "enum":
-	checkEnumMultipleValues(spec, idl);
-	checkSyntaxOfEnumValue(spec, idl);
-	break;
+        checkEnumMultipleValues(spec, idl);
+        checkSyntaxOfEnumValue(spec, idl);
+        break;
       }
     }
   }
