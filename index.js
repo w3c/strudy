1da2f5688434c3b168956fd0530ffb1966275302
François Daoust <fd@w3.org>
2023-05-24 15:12:45 +0200

    Expose studyWebIdl endpoint (#398)
    Will make it easier to access it from external projects that depend on Strudy.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   studyCrawl: require("./src/lib/study-crawl").studyCrawl,
+  studyWebIdl: require("./src/lib/study-webidl").studyWebIdl,
   generateReport: require("./src/lib/generate-report").generateReport,
 };
