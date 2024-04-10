802da110d8a70396229f527b8e5321d2d91a6df4
Francois Daoust <fd@tidoust.net>
2024-04-10 10:50:56 +0200

    Add Float16Array to the list of valid basic types
    Added to Web IDL in https://github.com/whatwg/webidl/pull/1398 and already used
    in the definition of `ArrayBufferView`.
    
    The test is not fantastically useful but will allow to make the test fail next
    time we need to add a basic type to the list.

--- src/lib/study-webidl.js
+++ src/lib/study-webidl.js
@@ -63,6 +63,7 @@ const basicTypes = new Set([
   'DOMString', // https://webidl.spec.whatwg.org/#idl-DOMString
   'double', // https://webidl.spec.whatwg.org/#idl-double
   'float', // https://webidl.spec.whatwg.org/#idl-float
+  'Float16Array', // https://webidl.spec.whatwg.org/#idl-Float16Array
   'Float32Array', // https://webidl.spec.whatwg.org/#idl-Float32Array
   'Float64Array', // https://webidl.spec.whatwg.org/#idl-Float64Array
   'Int16Array', // https://webidl.spec.whatwg.org/#idl-Int16Array
