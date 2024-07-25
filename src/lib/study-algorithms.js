dcb72e5d21649672fed47bd2ded9609e19a2e66b
François Daoust <fd@w3.org>
2024-07-25 12:02:31 +0200

    Add initial logic to study in parallel algorithm steps (#645)
    This initial logic reports algorithms that run steps in parallel, and that
    resolve/reject promises (or fire an event) within these steps without first
    queuing a task.
    
    The analysis logic remains very basic but manages to extract all problems that
    had been manually identified (with two exceptions detailed below):
      https://github.com/tidoust/parallel-promise/issues/1
    
    It also reports additional problems that had been missed in HTML, WebXR,
    Element Capture, Managed Configuration API, Viewport Capture and WebHID.
    
    The two exceptions are that are not yet detected automatically are:
    - indirect problems: when an algorithm step that runs in parallel calls another
    algorithm that contains steps that cannot run in parallel. This happens in
    Manifest Incubations:
    https://wicg.github.io/manifest-incubations/#dfn-steps-to-notify-that-an-install-prompt-is-available
    - use of "parallel queue" instead of "in parallel", in Web Locks API and
    Web Periodic Background Synchronization:
    https://w3c.github.io/web-locks/#ref-for-lock-task-queue
    https://wicg.github.io/periodic-background-sync/#process-periodic-sync-registrations-algorithm
    
    A couple of cases need to be hardcoded to account for uses of "in parallel" and
    "resolve" to mean something else in the Clipboard APIs, Push API and
    Prerendering Revamped specs.

---
+++ src/lib/study-algorithms.js
@@ -0,0 +1,156 @@
+const { JSDOM } = require('jsdom');
+const { recordCategorizedAnomaly } = require('./util');
+
+const possibleAnomalies = [
+  'missingTaskForPromise',
+  'missingTaskForEvent'
+];
+
+
+/**
+ * Normalize whitespaces in string to make analysis easier
+ */
+function normalize(str) {
+  return str.replace(/\r|\n/g, ' ').replace(/\s+/g, ' ').trim();
+}
+
+
+/**
+ * Return normalized prose from the given HTML string
+ */
+function normalizeHTML(html) {
+  const frag = JSDOM.fragment(html);
+  return normalize(frag.textContent);
+}
+
+/**
+ * Recursively look for algorithm steps that ask user agents to run remaining
+ * steps at the same algorithm level in parallel, and nest these remaining
+ * steps under them to ease processing afterwards.
+ */
+function nestParallelSteps(algo) {
+  if (algo.steps) {
+    for (let i = 0; i < algo.steps.length; i++) {
+      const step = algo.steps[i];
+      if (!step.html) {
+        continue;
+      }
+      if (step.steps) {
+        continue;
+      }
+      const prose = normalizeHTML(step.html);
+      if (prose.match(/(run|perform) the remaining steps in parallel/i) ||
+          prose.match(/continue running these steps in parallel/i) ||
+          prose.match(/assert: this( algorithm)? is running in parallel/i) ||
+          prose.match(/assert: these steps are running in parallel/i)) {
+        step.steps = algo.steps.splice(i + 1);
+        break;
+      }
+    }
+    for (const step of algo.steps) {
+      nestParallelSteps(step);
+    }
+  }
+}
+
+
+/**
+ * Main function, study all algorithms
+ */
+function studyAlgorithms(edResults) {
+  const report = [];
+  const recordAnomaly = recordCategorizedAnomaly(report, 'algorithms', possibleAnomalies);
+
+  // Return human-friendly markdown that identifies the given algorithm
+  function getAlgoName(algo) {
+    if (algo.name && algo.href) {
+      return `The [${algo.name}](${algo.href}) algorithm`;
+    }
+    else if (algo.href) {
+      return `The [algorithm](${algo.href})`;
+    }
+    else if (algo.name) {
+      return `The ${algo.name} algorithm`;
+    }
+    else if (algo.html) {
+      return `The algorithm that starts with "${normalizeHTML(algo.html)}"`;
+    }
+    else {
+      return 'An algorithm';
+    }
+  }
+
+  // Recursively find an anomaly in a step that runs in parallel
+  function findAnomalyInParallelStep(spec, algo, step) {
+    if (step.html) {
+      const html = normalize(step.html);
+      if (html.match(/queu(e|ing) an?( \w+){0,2} (micro)?task/i)) {
+        return false;
+      }
+      if (html.match(/(^|>| )(resolve|reject)(<| )/i) &&
+          // Push API checks on the status of promises:
+          // https://w3c.github.io/push-api/#receiving-a-push-message
+          !html.match(/(wait for|if) all( of)? the promises/i) &&
+          // Clipboard APIs uses "resolve" to mean something else:
+          // https://w3c.github.io/clipboard-apis/#dom-clipboard-read
+          !html.includes('systemClipboardRepresentation')
+      ) {
+        recordAnomaly(spec, 'missingTaskForPromise', `${getAlgoName(algo)} has a parallel step that resolves/rejects a promise directly`);
+        return true;
+      }
+      else if (html.match(/fire an?( \w+)? event/i)) {
+        recordAnomaly(spec, 'missingTaskForEvent', `${getAlgoName(algo)} has a parallel step that fires an event directly`);
+        return true;
+      }
+    }
+    let anomalyFound = false;
+    if (step.steps) {
+      anomalyFound = step.steps.find(substep => findAnomalyInParallelStep(spec, algo, substep));
+    }
+    return !!anomalyFound;
+  }
+
+  // Study an algorithm step
+  function studyAlgorithmStep(spec, algo, step) {
+    let anomalyFound = false;
+    if (step.html) {
+      const html = normalize(step.html);
+      if (html.match(/in parallel/i) &&
+          // Prerendering Revamped uses "in parallel" as an anchor for
+          // monkeypatching:
+          // https://wicg.github.io/nav-speculation/prerendering.html#patch-orientation-lock
+          !html.match(/before it goes <[^>]+>in parallel<\/a>/i)
+      ) {
+        anomalyFound = findAnomalyInParallelStep(spec, algo, step);
+        if (!anomalyFound) {
+          anomalyFound = !!step.steps?.find(substep => findAnomalyInParallelStep(spec, algo, substep));
+        }
+      }
+    }
+    if (!anomalyFound) {
+      anomalyFound = step.steps?.find(substep => studyAlgorithmStep(spec, algo, substep));
+    }
+    return anomalyFound;
+  }
+
+  // We're only interested in specs that define algorithms
+  const specs = edResults.filter(spec => !!spec.algorithms);
+
+  // Study algorithms in turn.
+  // Note: the root level of each algorithm is its first step. It may say
+  // something like "run these steps in parallel" in particular.
+  for (const spec of specs) {
+    for (const algo of spec.algorithms) {
+      nestParallelSteps(algo);
+      studyAlgorithmStep(spec, algo, algo);
+    }
+  }
+
+  return report;
+}
+
+
+/**************************************************
+Export methods for use as module
+**************************************************/
+module.exports = { studyAlgorithms };
