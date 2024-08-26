785e2c3af2301916dbdab017df14c71631571a3f
François Daoust <fd@w3.org>
2024-08-26 14:10:22 +0200

    Add --sort option to CLI (#678)
    Specs are sorted per URL in browser-specs. That order is not fantastic
    when preparing a human-readable report such as the one in:
    https://github.com/w3c/strudy/issues/663
    
    The --sort option lets makes it possible to specify the order to follow
    at each level of the structured report: one of "default", "name" or
    "title". We may want to add additional possibilities over time.

--- test/study.js
+++ test/study.js
@@ -191,4 +191,22 @@ describe('The main study function', function () {
 See [Dealing with the event loop](https://html.spec.whatwg.org/multipage/webappapis.html#event-loop-for-spec-authors) in the HTML specification for guidance on how to deal with algorithm sections that run *in parallel*.`
     });
   });
+
+  it('sorts entries as requested in the final report', async function() {
+    const crawlResult = [
+      populateSpec(specUrl, { error: 'Boo' }),
+      populateSpec(specUrl2, { error: 'Borked' })
+    ];
+    const report = await study(crawlResult, { structure: 'type/spec', sort: 'default/title', htmlFragments: {} });
+    assertNbAnomalies(report.results, 1);
+    assertAnomaly(report.results, 0, {
+      title: 'Crawl error',
+      content:
+`The following crawl errors occurred:
+* [Hello universe API](https://w3c.github.io/universe/)
+  * [ ] Borked
+* [Hello world API](https://w3c.github.io/world/)
+  * [ ] Boo`
+    });
+  });
 });
